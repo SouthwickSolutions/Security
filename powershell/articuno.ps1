df2233c57ea0a2bafe0d77d05aa72f1caf4617fb
Kevin Southwick <github@southwicksolutions.com>
2018-05-20 07:29:56 -0600

    20180520

--- powershell/articuno.ps1
+++ powershell/articuno.ps1
@@ -1,10 +1,20 @@
 ﻿<#
 Basic powershell startup script
 
-https://www.petri.com/run-powershell-scripts-with-administrative-privileges
+RUN SCRIPT AS ADMIN CODE - https://www.petri.com/run-powershell-scripts-with-administrative-privileges
+
+Script Tasks
+------------
+1.) start itself with admin privileges
+2.) start a transcript for the session (stored in default location)
+3.) imports the third party PSReadLine module
+4.) updates the help system
+5.) sets the starting location to C drive
+6.) displays the current powershell version
 
 #>
 
+#1.)
 #########START RUN AS ADMIN##########
 
 param([switch]$Elevated)
@@ -27,6 +37,17 @@ if((Check-Admin) -eq $false){
 
 ##########END RUN AS ADMIN##########
 
+#2.)
 Start-Transcript
+
+#3.)
 Import-Module PSReadLine
+
+#4.)
 Update-Help
+
+#5.)
+Set-Location -Path C:\
+
+#6.)
+$PSVersionTable.PSVersion
